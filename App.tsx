bf2d636bdec4cef119fce5ae8f0aab89e48acb12
Samuel Matias <hi@samuelematias.com>
2022-10-07 05:23:47 -0300

    feat: implement font.loadAsync

--- App.tsx
+++ App.tsx
@@ -1,7 +1,73 @@
+import {
+    useState,
+    useEffect,
+    useCallback,
+} from 'react';
+import { ViewProps } from 'react-native';
+
+import * as Font from 'expo-font';
+import {
+    Inter_900Black,
+    Inter_500Medium,
+} from '@expo-google-fonts/inter';
+import {
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+} from '@expo-google-fonts/rajdhani';
+import * as SplashScreen from 'expo-splash-screen';
+import styled from 'styled-components/native';
+
 import { SignIn } from './src/screens';
 
+const Container = styled.View<ViewProps>`
+  flex: 1;
+`;
+
 export default function App() {
+    const [appIsReady, setAppIsReady] = useState(false);
+
+    useEffect(() => {
+        async function prepare() {
+            try {
+                // Keep the splash screen visible while we fetch resources
+                await SplashScreen.preventAutoHideAsync();
+
+                // Pre-load fonts, make any API calls you need to do here
+                await Font.loadAsync({
+                    Inter_900Black,
+                    Inter_500Medium,
+                    Rajdhani_500Medium,
+                    Rajdhani_700Bold,
+                });
+
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                // Tell the application to render
+                setAppIsReady(true);
+            }
+        }
+
+        prepare();
+    }, []);
+
+    const onLayoutRootView = useCallback(async () => {
+        if (appIsReady) {
+            // This tells the splash screen to hide immediately! If we call this after
+            // `setAppIsReady`, then we may see a blank screen while the app is
+            // loading its initial state and rendering its first pixels. So instead,
+            // we hide the splash screen once we know the root view has already
+            // performed layout.
+            await SplashScreen.hideAsync();
+        }
+    }, [appIsReady]);
+
+    if (!appIsReady) {
+        return null;
+    }
     return (
-        <SignIn />
+        <Container onLayout={onLayoutRootView}>
+            <SignIn />
+        </Container>
     );
 }
