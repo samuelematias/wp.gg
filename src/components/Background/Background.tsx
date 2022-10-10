7bcaafe343571dbf1225cb551596d05a841f674e
Samuel Matias <hi@samuelematias.com>
2022-10-10 13:15:25 -0300

    refactor(component): improve Background code

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,20 +1,24 @@
 import { ReactNode } from 'react';
 
+import styled from 'styled-components/native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import { theme } from '../../global/styles';
 
-type BackgroundProps = {
+type Props = {
     children: ReactNode;
 }
 
+const StyledLinearGradient = styled(LinearGradient).attrs({
+    colors: [theme.colors.secondary80, theme.colors.secondary100],
+})`
+ `
 
-export function Background({ children }: BackgroundProps) {
-    const { secondary80, secondary100 } = theme.colors;
+export function Background({ children }: Props) {
 
     return (
-        <LinearGradient colors={[secondary80, secondary100]}>
+        <StyledLinearGradient>
             {children}
-        </LinearGradient>
+        </StyledLinearGradient>
     );
 }
