a5be01ea7e8e184105f84c0b9677e28fe7898503
Samuel Matias <hi@samuelematias.com>
2022-10-14 07:42:03 -0300

    style(component): improve ListDivider style

--- src/components/ListDivider/ListDivider.tsx
+++ src/components/ListDivider/ListDivider.tsx
@@ -2,16 +2,17 @@ import styled from 'styled-components/native';
 
 import { theme } from '../../global/styles';
 
-const Container = styled.View`
-    width: 80%;
+const Divider = styled.View`
+    width: 78%;
     height: 1px;
     background-color: ${theme.colors.secondary40};
-    margin-vertical: 21px;
+    margin-top: 2px;
+    margin-vertical: 31px;
     align-self: flex-end;
 `;
 
 export function ListDivider() {
     return (
-        <Container />
+        <Divider />
     );
 }
