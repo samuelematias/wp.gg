152fa9acfabffddeec7c339785c0f58260d00cdc
Samuel Matias <hi@samuelematias.com>
2022-10-15 20:46:50 -0300

    feat(auth): create hook for useContext/config auth

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,109 @@
+import React,
+{
+    createContext,
+    useState,
+    ReactNode,
+    useContext,
+} from 'react';
+
+import * as AuthSession from 'expo-auth-session';
+import {
+    REDIRECT_URI,
+    SCOPE,
+    RESPONSE_TYPE,
+    CLIENT_ID,
+    CDN_IMAGE
+} from '../config';
+import { api } from '../services';
+
+type User = {
+    id: string;
+    userName: string;
+    firstName: string;
+    avatar: string;
+    email: string;
+    token: string;
+};
+
+type AuthContextData = {
+    user: User | null;
+    loading: boolean;
+    signIn: () => Promise<void>;
+};
+
+type AuthProviderProps = {
+    children: ReactNode;
+}
+
+type AuthorizationResponse = AuthSession.AuthSessionResult & {
+    params: {
+        access_token?: string;
+        error?: string;
+    }
+}
+
+const AuthContext = createContext<AuthContextData>({
+    user: null,
+    loading: false,
+    signIn: () => Promise.resolve(),
+});
+
+function AuthProvider({ children }: AuthProviderProps) {
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    async function signIn() {
+        try {
+            setLoading(true);
+
+            const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+            const { type, params } = await AuthSession
+                .startAsync({ authUrl }) as AuthorizationResponse;
+
+            if (type === "success" && !params.error) {
+                api.defaults.headers.authorization = `Bearer ${params.access_token}`;
+
+                const userInfo = await api.get('/users/@me');
+
+                const firstName = userInfo.data.username.split(' ')[0];
+                userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
+
+                const userData = {
+                    ...userInfo.data,
+                    firstName,
+                    token: params.access_token
+                }
+
+                /* await AsyncStorage.setItem(COLLECTION_USERS, JSON.stringify(userData)); */
+                setUser(userData);
+            }
+
+        } catch {
+            throw new Error('Não foi possível autenticar');
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            user,
+            loading,
+            signIn,
+        }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+function useAuth() {
+    const context = useContext(AuthContext);
+
+    return context;
+}
+
+export {
+    AuthProvider,
+    useAuth,
+};
