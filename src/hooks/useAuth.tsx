571df884a4aec2469011026b1bfeb8831f46bd0c
Samuel Matias <hi@samuelematias.com>
2022-10-16 13:13:07 -0300

    feat(dotenv): implement dotenv on useAuth

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -8,13 +8,13 @@ import React,
 
 import * as AuthSession from 'expo-auth-session';
 
-import {
-    REDIRECT_URI,
-    SCOPE,
-    RESPONSE_TYPE,
-    CLIENT_ID,
-    CDN_IMAGE
-} from '../config';
+
+const { SCOPE } = process.env;
+const { CLIENT_ID } = process.env;
+const { CDN_IMAGE } = process.env;
+const { REDIRECT_URI } = process.env;
+const { RESPONSE_TYPE } = process.env;
+
 import { api } from '../services';
 
 type User = {
