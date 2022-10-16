d126361891afad7fb7257cb6aa99974e0a969ede
Samuel Matias <hi@samuelematias.com>
2022-10-16 13:12:01 -0300

    chore(lib): add dotenv libs

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       "react-native-reanimated/plugin", // has to be listed last according to the documentation. https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/#babel-plugin
+      "inline-dotenv",
     ],
   };
 };
